07b04523d32fc00da8cb1a4be718e43e087e4bf4
cog042 <cog042@gmail.com>
2017-01-27 11:24:43 -0600

    Add project files.

---
+++ JSON Payment Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JSON_Payment_Portal.Startup))]
+namespace JSON_Payment_Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
