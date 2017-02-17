59875b37339f32973a6386561a48f770450194d4
cog042 <cog042@gmail.com>
2017-02-16 22:21:19 -0600

    added dashboard models and view

---
+++ JSON Payment Portal/Controllers/DashboardController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace FinancingAPI.Controllers
+{
+    public class DashboardController : Controller
+    {
+
+
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return RedirectToAction("");
+        }
+
+        public ActionResult ProjectOverview()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectFinancing()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectSite()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectInstallation()
+        {
+
+            return View();
+        }
+
+        public ActionResult ProjectDocuments()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectExperience()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectMonitoring()
+        {
+            return View();
+        }
+
+        public ActionResult ProjectPayment()
+        {
+            return View();
+        }
+
+
+
+
+
+        // GET: Dashboard/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
+            return View();
+        }
+
+        // GET: Dashboard/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+
+        // POST: Dashboard/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+
+        // GET: Dashboard/Delete/5
+    }
+}
---
+++ JSON Payment Portal/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JSON_Payment_Portal.Models
+{
+    public class Project
+    {   
+        public int projectNum { get; set; }
+        public int referenceNumb { get; set; }
+        public int SSN { get; set; }
+        public int LoanLeaseOption { get; set; }
+        public int finalOption { get; set; }
+
+    }
+}
