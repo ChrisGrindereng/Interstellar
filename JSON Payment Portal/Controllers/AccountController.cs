9cb434097ddc737e7fe58de5dac3f5f15c6af3d1
cog042 <cog042@gmail.com>
2017-02-27 22:30:02 -0600

    production site changes

--- JSON Payment Portal/Controllers/AccountController.cs
+++ JSON Payment Portal/Controllers/AccountController.cs
@@ -72,7 +72,7 @@ namespace JSON_Payment_Portal.Controllers
             {
                 return View(model);
             }
-
+            return RedirectToAction("../Dashboard/ProjectOverview");
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
